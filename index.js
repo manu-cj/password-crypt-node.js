87e7a9106669ff48c19f8dc4ab4ff597778823a3
manu-cj <carenjotje@gmail.com>
2024-03-23 18:24:56 +0100

    bcrypt compare renvoie toujours false, à réglé

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ const app = express();
 port = 8000;
 
 
+const connect = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'node'
+})
+
+const query = async (query, values) => {
+    const [rows] = await connect.execute(query, values);
+    return rows
+}
+
 //requête sur le serveur
 app.get('/', (req, res) => {
     res.send("Hello world");
@@ -13,30 +25,57 @@ app.get('/', (req, res) => {
 
 //à modifier en post pour récupérer les données envoyé d'une autres page, async pour pouvoir utiliser await
 app.get('/add', async (req, res) => {
-    const connect = mysql.createPool({
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'node'
-    })
-
-    const query = async (query, values) => {
-        const [rows] = await connect.execute(query, values);
-        return rows
-    }
+    
 
 // données qu'on souhaite ajouté en bdd
-const username = "Marc";
+const username = "Poo";
 const mail = "marc@secure.com";
-const pass = "mot de passe sécurisé";
+const pass = "";
 // cryptage du mot de passe
-const passHash = await bcrypt.hash(pass, 10);
+const passHash = await bcrypt.hash(pass, 5);
+
+bcrypt.compare(pass, passHash, (err, result) => {
+    console.log(result); 
+})
 
 
 await query("INSERT INTO users (id, pseudo, mail, password) VALUES (null, ?, ?, ?)", [username, mail, passHash]);
 
 res.json({add: true});
 })
+
+
+app.get('/auth', async (req, res) => {
+    const pseudo = "Poo";
+    const pass2 = "";
+
+    try {
+        const [user] = await query("SELECT * FROM users WHERE pseudo = ?", [pseudo]);
+        console.log("User:", user);
+
+        if (!user) {
+            console.log("User not found in database.");
+            return res.json({ auth: false });
+        }
+//return toujours false à réglé !
+        const result = await bcrypt.compare(pass2, user.password);
+        console.log("Authentication result:", result);
+
+        if (result) {
+            console.log("Authentication successful!");
+            res.json({ auth: true });
+        } else {
+            console.log("Authentication failed! Password does not match.");
+            res.json({ auth: false });
+        }
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+
+
 
 
 
